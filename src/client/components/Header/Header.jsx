20c7d5d39d4914ba34079ba52818570dbd605f6c
Madison Lisle <madmaxbeyond.nonrevolution@slmail.me>
2023-10-19 15:24:33 -0700

    Remove div from header

--- src/client/components/Header/Header.jsx
+++ src/client/components/Header/Header.jsx
@@ -3,9 +3,7 @@ import headerCurve from '/header-curve.svg'
 const Header = () => {
 
     return (
-        <div className='header'>
-            <img src={headerCurve} className='svg-curve-header' alt='header curve' />
-        </div>
+        <img src={headerCurve} className='svg-curve-header' alt='header curve' />
     );
 };
 
